e2e2cf8c6660dea668f190510edf5fb003b3e06d
Magnus Woldrich <magnus.woldrich@gmail.com>
2021-07-07 10:11:09 +0200

    initial

---
+++ README.md
@@ -0,0 +1,48 @@
+beets-titlecase_proper
+----------------------
+
+Adds a template function `%titlecase_proper{}` that titlecases
+strings better.
+
+The beets builtin template function `%title{}` uses `str.capwords()`.
+This causes issues with strings that begin with a character that can't
+be titlecased:
+
+`
+It's Raining Men (instrumental Version).flac
+`
+
+`str.title()` does a better job but causes issues with strings like
+"It's" instead:
+
+`
+It'S Raining Men (Instrumental Version).flac
+`
+
+This plugin takes care of both issues.
+
+Installation
+------------
+
+Replace all `%title{}` functions with the shiny new one:
+
+`
+perl -i.old -pe 's/%title{(\$[A-Za-z_-]+)}/%titlecase_proper{$1}/g' config.yaml
+`
+
+Add this script to the beets pluginpath:
+
+
+`
+pluginpath: ['/usr/lib/python3.9/site-packages/beetsplug/',
+             '/home/scp1/dev/beets-titlecase_proper',
+            ]
+`
+
+Add the plugin to the plugin list:
+
+`
+plugins: [
+  'titlecase_proper'
+]
+`
---
+++ titlecase_proper.py
@@ -0,0 +1,32 @@
+# Author: Magnus Woldrich, japh@irc.libera.chat
+#   Date: 2021-07-07 10:07:37
+# > https://github.com/beetbox/beets/discussions/3998
+#
+# adds a template function %titlecase_proper{} that titlecases
+# strings better.
+#
+# The beets builtin template function %title{} uses str.capwords(). This
+# causes issues with strings that begin with a character that can't be
+# titlecased:
+#
+# It's Raining Men (instrumental Version).flac
+#
+# str.title() does a better job but causes issues with strings like
+# "It's" instead:
+#
+# It'S Raining Men (Instrumental Version).flac
+#
+# This plugin takes care of both issues.
+
+from beets.plugins import BeetsPlugin
+
+class titlecase_proper(BeetsPlugin):
+    def __init__(self):
+        super(titlecase_proper, self).__init__()
+        self.template_funcs['titlecase_proper'] = _titlecase_proper
+
+def _titlecase_proper(text):
+   import re
+   return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
+                    lambda mo: mo.group(0).capitalize(),
+                     text)
