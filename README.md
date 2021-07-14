f62bf87e277148a2f95791a33e6c9b320164dc28
Magnus Woldrich <magnus.woldrich@gmail.com>
2021-07-14 08:46:53 +0200

    add ability to configure words that should be returned as-is

--- README.md
+++ README.md
@@ -8,43 +8,62 @@ The beets builtin template function `%title{}` uses `str.capwords()`.
 This causes issues with strings that begin with a character that can't
 be titlecased:
 
-`
-It's Raining Men (instrumental Version).flac
-`
+
+> It's Raining Men (instrumental Version).flac
+
 
 `str.title()` does a better job but causes issues with strings like
 "It's" instead:
 
-`
-It'S Raining Men (Instrumental Version).flac
-`
+
+> It'S Raining Men (Instrumental Version).flac
+
 
 This plugin takes care of both issues.
 
+Additionally, words can be added to **~/.beets\_titlecase** that will be
+returned as-is:
+
+
+```bash
+{% highlight bash %}
+$ cd "Dj TCP the Reckless │2021│ So Much Drama in the PHD [CDR, MP3]"
+$ for w in TCP DJ PhD; do echo $w; done >> ~/.beets_titlecase
+$ beet import .
+$ beet ls -a 'so much drama' -f \$path
+DJ TCP the Reckless │2021│ So Much Drama in the PhD [CDR, MP3]
+```
+
+
 Installation
 ------------
-
-`$ pip3 install titlecase`
+```bash
+$ pip3 install titlecase
+```
 
 Replace all `%title{}` functions with the shiny new one:
 
-`
+```bash
 perl -i.old -pe 's/%title{(\$[A-Za-z_-]+)}/%titlecase_proper{$1}/g' config.yaml
-`
+```
 
 Add this script to the beets pluginpath:
 
-
-`
+```yaml
 pluginpath: ['/usr/lib/python3.9/site-packages/beetsplug/',
              '/home/scp1/dev/beets-titlecase_proper',
             ]
-`
+```
 
 Add the plugin to the plugin list:
 
-`
+```yaml
 plugins: [
   'titlecase_proper'
 ]
-`
+```
+
+TODO
+----
+Replace the text file in ~/ with inline configuration in config.yaml:
+https://beets.readthedocs.io/en/stable/dev/plugins.html#read-configuration-options
--- titlecase_proper.py
+++ titlecase_proper.py
@@ -17,9 +17,46 @@
 # It'S Raining Men (Instrumental Version).flac
 #
 # This plugin takes care of both issues.
+# Furthermore, word specified in ~/.beets_titlecase
 
 from beets.plugins import BeetsPlugin
-from titlecase import titlecase
+from titlecase     import titlecase
+import os
+import logging
+logger = logging.getLogger(__name__)
+
+# wordlist with words that will be returned as-is and NOT titlecased.
+# ~/.beets_titlecase
+# That means situations like this can be avoided:
+# > Dj TCP the Reckless │2021│ So Much Drama in the PHD [CDR, MP3]
+#
+# And instead we get the proper:
+# > DJ TCP the Reckless │2021│ So Much Drama in the PhD [CDR, MP3]
+wordlist_asis = os.path.join(os.path.expanduser('~'), '.beets_titlecase')
+
+def create_wordlist_filter_from_file(file_path):
+    '''
+    Load a list of abbreviations from the file with the provided path,
+    reading one abbreviation from each line, and return a callback to
+    be passed to the `titlecase` function for preserving their given
+    canonical capitalization during title-casing.
+    '''
+    if file_path is None:
+        logger.debug('No abbreviations file path given')
+        return lambda word, **kwargs: None
+    file_path_str = str(file_path)
+    if not os.path.isfile(file_path_str):
+        logger.debug('No abbreviations file found at ' + file_path_str)
+        return lambda word, **kwargs: None
+    with open(file_path_str) as f:
+        logger.debug('Reading abbreviations from file ' + file_path_str)
+        abbrevs_gen = (line.strip() for line in f.read().splitlines() if line)
+        abbrevs = {abbr.upper(): abbr for abbr in abbrevs_gen}
+        if logger.isEnabledFor(logging.DEBUG):
+            for abbr in abbrevs.values():
+                logger.debug('Registered abbreviation: ' + abbr)
+        return lambda word, **kwargs: abbrevs.get(word.upper())
+
 
 class titlecase_proper(BeetsPlugin):
     def __init__(self):
@@ -27,8 +64,4 @@ class titlecase_proper(BeetsPlugin):
         self.template_funcs['titlecase_proper'] = _titlecase_proper
 
 def _titlecase_proper(text):
-  return titlecase(text)
-#   import re
-#   return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
-#                    lambda mo: mo.group(0).capitalize(),
-#                     text)
+  return titlecase(text, callback=create_wordlist_filter_from_file(wordlist_asis))
